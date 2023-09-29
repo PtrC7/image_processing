d2f9918f5eb08a023374b93c3bb287f4ecdcaf48
PtrC7 <petechaya7@gmail.com>
2023-09-28 22:49:02 -0400

    Finished histoMatch

--- hw1/HW_histoMatch.cpp
+++ hw1/HW_histoMatch.cpp
@@ -97,68 +97,63 @@ histoMatchApprox(ImagePtr I1, ImagePtr targetHisto, ImagePtr I2)
 {
 	// copy image header (width, height) of input image I1 to output image I2
 	IP_copyImageHeader(I1, I2);
-	int i, lut[MXGRAY];
-	int total, h1[MXGRAY];
+	int i, p, R = 0;
+	int left[MXGRAY], right[MXGRAY];
+	int total, Hsum = 0, Havg = 0, h1[MXGRAY];
 	ChannelPtr<uchar> in, out;
 	ChannelPtr<int> h2;
+	double scale;
 	int type;
-	int normalH[MXGRAY], normalT[MXGRAY], runningH[MXGRAY], runningT[MXGRAY];
 
 	// init vars for width, height, and total number of pixels
 	int w = I1->width();
 	int h = I1->height();
 	total = w * h;
 
-	/*
-	int Havg = 0;
-	IP_getChannel(targetHisto, 0, h2, type);
-	for (i = 0; i < MXGRAY; i++) {
-		Havg += h2[i];
-	}
-	double scale = (double)total / Havg;
+	// clear histogram
+	for (i = 0; i < MXGRAY; i++)
+		h1[i] = 0;
 
-	if (scale != 1) {
-		for (i = 0; i < MXGRAY; i++) {
-			h2[i] *= scale;
-		}
-	}*/
-
-	// compute normalized histogram
+	// eval histogram
 	IP_getChannel(I1, 0, in, type);
-	for (i = 0; i < MXGRAY; i++)
-		h1[i] = 0; // clear
 	for (i = 0; i < total; i++)
-		h1[in[i]]++; // eval
-	for (i = 0; i < MXGRAY; i++)
-		normalH[i] = h1[i] / total; // normalized histogram
+		h1[in[i]]++;
 
-	// normalize target histogram
+	// target histogram
 	IP_getChannel(targetHisto, 0, h2, type);
-	for (i = 0; i < MXGRAY; i++)
-		 normalT[i] = h2[i] / total;
 
-	// compute the cdf of the image (risingsum(h))
-	runningH[0] = normalH[0];
-	for (i = 1; i < MXGRAY; i++)
-		runningH[i] = runningH[i - 1] + normalH[i];
+	// normalize h2 to conform with dimensions of I1
+	for (i = Havg = 0; i < MXGRAY; i++) Havg += h2[i];
+	scale = (double)total / Havg;
+	if (scale != 1)
+		for (i = 0; i < MXGRAY; i++)
+			h2[i] *= scale;
 
-	// compute the cdf of href
-	runningT[0] = normalT[0];
-	for (i = 1; i < MXGRAY; i++)
-		runningT[i] = runningT[i - 1] + normalT[i];
-	
+	// Evaluate remapping of all input gray levels
 	for (i = 0; i < MXGRAY; i++) {
-		int l = 255;
-		do {
-			lut[i] = l;
-			l--;
-		} while (l >= 0 && runningT[l] > runningH[i]);
+		left[i] = R;
+		Hsum += h1[i];
+		while (Hsum > h2[R] && R < MXGRAY - 1) {
+			Hsum -= h2[R];
+			R++;
+		}
+		right[i] = R;
 	}
 
-	// visit all image channels and evaluate output image
-	for (int ch = 0; IP_getChannel(I1, ch, in, type); ch++) {	// get input  pointer for channel ch
-		IP_getChannel(I2, ch, out, type);		// get output pointer for channel ch
-		for (i = 0; i < total; i++) 
-			*out++ = lut[*in++];	// use lut[] to eval output
+	// clear h1 and reuse it
+	for (i = 0; i < MXGRAY; i++)
+		h1[i] = 0;
+
+	// visit all input pixels
+	for (int ch = 0; IP_getChannel(I1, ch, in, type); ch++) {
+		IP_getChannel(I2, ch, out, type);
+
+		for (i = 0; i < total; i++) {
+			p = left[in[i]];
+			if (h1[p] < h2[p])
+				out[i] = p;
+			else out[i] = p = left[in[i]] = MIN(p + 1, right[in[i]]);
+			h1[p]++;
+		}
 	}
 }
